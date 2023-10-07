82d631316a5e7a1ebcc9947751f064b2bc29a509
srinivas <srinivasbharatha007@gmail.com>
2023-10-08 01:04:30 +0530

    Refactored and updated code to show past messages on chat window and messages on alert box

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -24,32 +24,8 @@ const submit = (event) =>{
     event.preventDefault();
 }
 
-function codingCourse() {
-  console.log("freeCodeCamp");
-}
-
-
-const onConnectedSocket =(event) =>{
-    getAndShowPastMessages();
-    console.log("start->" + new Date());
-    //window.setTimeout(codingCourse, 3000);
-    setTimeout(onConnected, 500)
-    //const myTimeout = setTimeout(codingCourse, 5000);
-    //myStopFunction();
-  // (async () => await new Promise(resolve => window.setTimeout(resolve, 2000)))();
-    console.log("end->" + new Date());
-    //event.preventDefault();
-}
-
 const onConnected=(event) =>{
-   // getAndShowPastMessages();
-   // console.log("start->" + new Date());
-    //window.setTimeout(codingCourse, 3000);
-     //window.setTimeout(resolve, 2000)
-    //const myTimeout = setTimeout(codingCourse, 5000);
-    //myStopFunction();
-   //(async () => await new Promise(resolve => window.setTimeout(resolve, 2000)))();
-  ///  console.log("end->" + new Date());
+    getAndShowPastMessages();
     stompClient.subscribe('/topic/public',OnMessageReceived);
     var message = {
         senderName: userName,
@@ -57,31 +33,29 @@ const onConnected=(event) =>{
     }
     stompClient.send('/app/addUser',{}, JSON.stringify(message));
     connecting.classList.add('hidden');
-
     //event.preventDefault();
 }
 
-//const audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
-//short-success-sound-glockenspiel-treasure-video-game-6346
-// Replace with the path to your sound file
-
 const notifyAudio = new Audio('short-success-sound.mp3');
-
 
 const OnMessageReceived =(payload)=>{
     var inputMessage =JSON.parse(payload.body);
     var messageElement = document.createElement("li");
-    //alert('New message received!')
     var alertMessage ="New message received!";
     if(inputMessage.status==='JOIN'){
         messageElement.classList.add("event-message")
-        inputMessage.message=inputMessage.senderName + " joined!";
+        if(userName===inputMessage.senderName){
+            inputMessage.message="  You joined!";
+        }
+        else{
+            inputMessage.message=inputMessage.senderName + " joined!";
+        }
         alertMessage="New user "+inputMessage.senderName+" joined!";
     }
     else if(inputMessage.status==='LEAVE'){
         messageElement.classList.add("event-message")
-        inputMessage.message=inputMessage.senderName + "left!";
-        alertMessage=inputMessage.senderName + "left!";
+        inputMessage.message=inputMessage.senderName + " left!";
+        alertMessage=inputMessage.senderName + " left!";
     }
     else{
         messageElement.classList.add("chat-message")
@@ -97,6 +71,8 @@ const OnMessageReceived =(payload)=>{
         var userNameElementText = document.createTextNode(inputMessage.senderName);
         userNameElement.appendChild(userNameElementText);
         messageElement.appendChild(userNameElement);
+
+        alertMessage+="\n"+inputMessage.senderName+" : " + inputMessage.message;
     }
 
         var textElement = document.createElement("p");
@@ -148,17 +124,12 @@ const sendMessage =(event)=>{
 userNameForm.addEventListener('submit',submit, true);
 messageForm.addEventListener('submit',sendMessage, true);
 
-const myAsyncFunction = async () => {
-  await getAndShowPastMessages();
-  // Code here will not execute until someAsyncFunction completes.
-};
-
 const getAndShowPastMessages = async () => {
    const getMessagesApiUrl = "http://localhost:8080/api/get";
    const res = await fetch(getMessagesApiUrl);
    if (res.ok) {
      const messages = await res.json();
-     await messages.forEach(m => showMessage(m));
+     messages.forEach(m => showMessage(m));
    } else {
      console.log("Unable to fetch past messages!");
    }
@@ -167,30 +138,19 @@ const getAndShowPastMessages = async () => {
 const showMessage = (inputMessage) => {
      console.log("input messages ....."+inputMessage)
      var messageElement = document.createElement("li");
-     if(inputMessage.status==='JOIN'){
-         messageElement.classList.add("event-message")
-         inputMessage.message=inputMessage.senderName + "joined!";
-     }
-     else if(inputMessage.status==='LEAVE'){
-         messageElement.classList.add("event-message")
-         inputMessage.message=inputMessage.senderName + "left!";
-     }
-     else{
-         messageElement.classList.add("chat-message")
+     messageElement.classList.add("chat-message")
 
-         var avatar = document.createElement("i");
-         var avatarText = document.createTextNode(inputMessage.senderName[0]);
-         avatar.appendChild(avatarText);
-         avatar.style['background-color'] = '#ff85af';
-         //avatarElement.style['background-color'] = getAvatarColor(message.sender);
-         messageElement.appendChild(avatar);
+     var avatar = document.createElement("i");
+     var avatarText = document.createTextNode(inputMessage.senderName[0]);
+     avatar.appendChild(avatarText);
+     avatar.style['background-color'] = '#ff85af';
+     //avatarElement.style['background-color'] = getAvatarColor(message.sender);
+     messageElement.appendChild(avatar);
 
-         var userNameElement = document.createElement("span");
-         var userNameElementText = document.createTextNode(inputMessage.senderName);
-         userNameElement.appendChild(userNameElementText);
-         messageElement.appendChild(userNameElement);
-     }
-
+     var userNameElement = document.createElement("span");
+     var userNameElementText = document.createTextNode(inputMessage.senderName);
+     userNameElement.appendChild(userNameElementText);
+     messageElement.appendChild(userNameElement);
      var textElement = document.createElement("p");
      var messageText = document.createTextNode(inputMessage.message);
      textElement.appendChild(messageText);
@@ -199,7 +159,6 @@ const showMessage = (inputMessage) => {
      var date = document.createTextNode(inputMessage.date+' ');
      dateElement.appendChild(date);
      dateElement.style.fontSize = "8px";
-     dateElement.style.color = '#edede8';
 
      messageElement.appendChild(textElement);
      messageElement.appendChild(dateElement);
